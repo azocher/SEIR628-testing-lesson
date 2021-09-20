11eda754c62d4111ab8d94014e318afd120c0b9c
Anna Zocher <anna.zocher@gmail.com>
2021-09-20 11:23:02 -0700

    added counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,9 +5,20 @@ class Counter extends Component {
         counter: 0
     }
 
+    handleClick = () => {
+        let updatedCounter = this.state.counter + 1
+        console.log(updatedCounter)
+        this.setState({
+            counter: updatedCounter
+        })
+    }
+
     render() {
         return (
-            <div className="number">Current Count: {this.state.counter}</div>
+            <div>
+                <div className="number">Current Count: {this.state.counter}</div>
+                <button className="plus" onClick={this.handleClick}>Click Here</button>
+            </div>
         )
     }
 }
